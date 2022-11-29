7d3924d340c5b671bbd20becde7e30f26695c9e1
= <mmattmmo@gmail.com>
2022-11-29 12:59:47 +0100

    cryptographie-14

--- cryptographie.py
+++ cryptographie.py
@@ -1,23 +1,25 @@
+#Par Mattéo Menou
 from pygame import * #Import de tout les éléments de pygame
 from sys import * #Importer tout les éléments de sys
 import pygame #Import de pygame
 pygame.init() #Lancement de pygame
 from MLib import *
+from cryptage_ROT13 import * #Imporer le code ROT13 fait par Alexis Bergère
 
 TAILLE=(500, 500) #Taille de la fenêtre
 
 fenetre=display.set_mode(TAILLE) #Création de l'instance d'une fenêtre
 app = MFenetre(fenetre, "Cryptographie", arrierePlanImage="assets/fond.gif", arrierePlanImageAlignement="CJ", arrierePlanImageParSeconde=48) #Création d'un objet app pour mieux gérer la fenêtre
 
-titre = MTexte("Chiffrage", (100, 5), (300, 90), parent=app, bordureCouleur=(0, 0, 0), bordureLargeur=3, bordureRayon=25, arrierePlanCouleur=(255, 255, 255, 255*0.75), policeTaille=65, texteAlignement="CC") #Titre de la fenêtre
-texteDecrypter = MEntreeTexte((100, 100), (300, 90), app, ligneLongueurMax=280, ligneMax=3, bordureRayon=15, policeTaille=22, policeType="defaut", texteAlignement="GH") #Entrée du texte déchiffré
-boutonCrypter = MBouton((10, 210), (150, 60), app, actionAuSurvol="policeTaille=32", texte="Chiffrer", texteAlignement="CC", policeTaille=24, policeType="defaut", bordureLargeur=2, bordureRayon=5) #Bouton pour mettre le texte déchiffré dans l'entrée du texte chiffré après chiffrage
-boutonDecrypter = MBouton((340, 210), (150, 60), app, actionAuSurvol="policeTaille=32", texte="Déchiffrer", texteAlignement="CC", policeTaille=24, bordureLargeur=2, bordureRayon=5) #Bouton pour mettre le texte chiffré dans l'entrée du texte déchiffré après déchiffrage
-bordureChoixBouton = MTexte("Rot13", (170, 195), (160, 80), app, arrierePlanCouleur=(255, 255, 255, 255*0.75), bordureLargeur=2, bordureRayon=15, policeTaille=24, texteAlignement="GC") #Texte qui contient le nom du chiffrage utilisé
-boutonGaucheChoixBouton = MBouton((87, 20), (35, 40), bordureChoixBouton, "", "<", bordureLargeur=2, policeTaille=30, texteAlignement="CC") #Bouton pour changer le chiffrage
-boutonDroiteChoixBouton = MBouton((122, 20), (35, 40), bordureChoixBouton, "", ">", bordureLargeur=2, policeTaille=30, texteAlignement="CC") #Bouton pour changer le chiffrage
-texteCleVigenere = MEntreeTexte((170, 280), (160, 35), app, bordureLargeur=4, caracteresAuthorises=(ALPHA_UPPER), policeTaille=20, texteAlignement="CC") #Clé de Vigenère (disponible que quand le chiffrage est à Vigenère)
-texteCrypter = MEntreeTexte((100, 320), (300, 90), app, ligneLongueurMax=280, ligneMax=3, bordureRayon=15, policeTaille=22, texteAlignement="GH") #Entrée du texte chiffré
+titre = MTexte(texte="Chiffrage", position=(100, 5), taille=(300, 90), parent=app, bordureCouleur=(0, 0, 0), bordureLargeur=3, bordureRayon=25, arrierePlanCouleur=(255, 255, 255, 255*0.75), policeTaille=65, texteAlignement="CC") #Titre de la fenêtre
+texteDecrypter = MEntreeTexte(position=(100, 100), taille=(300, 90), parent=app, ligneLongueurMax=280, ligneMax=3, bordureRayon=15, policeTaille=22, policeType="defaut", texteAlignement="GH") #Entrée du texte déchiffré
+boutonCrypter = MBouton(position=(10, 210), taille=(150, 60), parent=app, actionAuSurvol="policeTaille=32", texte="Chiffrer", texteAlignement="CC", policeTaille=24, policeType="defaut", bordureLargeur=2, bordureRayon=5) #Bouton pour mettre le texte déchiffré dans l'entrée du texte chiffré après chiffrage
+boutonDecrypter = MBouton(position=(340, 210), taille=(150, 60), parent=app, actionAuSurvol="policeTaille=32", texte="Déchiffrer", texteAlignement="CC", policeTaille=24, bordureLargeur=2, bordureRayon=5) #Bouton pour mettre le texte chiffré dans l'entrée du texte déchiffré après déchiffrage
+bordureChoixBouton = MTexte(texte="Rot13", position=(170, 195), taille=(160, 80), parent=app, arrierePlanCouleur=(255, 255, 255, 255*0.75), bordureLargeur=2, bordureRayon=15, policeTaille=24, texteAlignement="GC") #Texte qui contient le nom du chiffrage utilisé
+boutonGaucheChoixBouton = MBouton(position=(87, 20), taille=(35, 40), parent=bordureChoixBouton, actionAuSurvol="", texte="<", bordureLargeur=2, policeTaille=30, texteAlignement="CC") #Bouton pour changer le chiffrage
+boutonDroiteChoixBouton = MBouton(position=(122, 20), taille=(35, 40), parent=bordureChoixBouton, actionAuSurvol="", texte=">", bordureLargeur=2, policeTaille=30, texteAlignement="CC") #Bouton pour changer le chiffrage
+texteCleVigenere = MEntreeTexte(position=(170, 280), taille=(160, 35), parent=app, bordureLargeur=4, caracteresAuthorises=(ALPHA_UPPER), policeTaille=20, texteAlignement="CC") #Clé de Vigenère (disponible que quand le chiffrage est à Vigenère)
+texteCrypter = MEntreeTexte(position=(100, 320), taille=(300, 90), parent=app, ligneLongueurMax=280, ligneMax=3, bordureRayon=15, policeTaille=22, texteAlignement="GH") #Entrée du texte chiffré
 
 while True: #Boucle infini tant que l'évènement "quitter" n'est pas vue
     app.frame()
@@ -36,19 +38,7 @@ while True: #Boucle infini tant que l'évènement "quitter" n'est pas vue
     if boutonCrypter.get_clicke(): #Crypter le texte
         if bordureChoixBouton.get_texte() == "Rot13": #Avec rot13
             texte = texteDecrypter.get_texte() #Obtention du texte à crypter
-            resultat = ""
-            for c in texte: #Analyse de chaque caractères
-                if strAlpha(c): #Si le caractère est dans l'alphabet
-                    entier = ord(c) + 13 #Applicage de rot13 sur tout les caractères
-                    if strAlphaLower(c):
-                        if entier > 122:
-                            entier -= 26
-                    elif strAlphaUpper(c):
-                        if entier > 91:
-                            entier -= 26
-                    resultat += chr(entier) #Appliquage au résultat
-                else: #Si il n'est pas dans l'alphabet
-                    resultat += c
+            resultat = rot13(texte)
             texteCrypter.set_texte(resultat) #Appliquage du résultat final
         else: #Avec Vigenère
             texte = texteDecrypter.get_texte() #Obtention du texte à crypter
